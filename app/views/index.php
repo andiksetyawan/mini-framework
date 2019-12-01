d9f34a495db0c2479501feb44c348a18585b3de9
Andik Setyawan <andik.setyawan19@gmail.com>
2019-12-01 20:35:36 +0700

    dstore

--- app/views/index.php
+++ app/views/index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Mini Framework Home</title>
     <script src="./assets/js/test.js" charset="utf-8"></script>
   </head>
   <body>
